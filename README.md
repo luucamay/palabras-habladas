c560cfa3b52d562ef3a30c8d0abf2e10329de29a
Lupe <lupemaydana@gmail.com>
2023-08-10 17:09:22 -0400

    feat: Add interaction with api

--- README.md
+++ README.md
@@ -7,3 +7,8 @@ A web app to clone your voice using eleven labs API
 - Call the api to get a voice and generate audio to be played on the website
 - Review difference between audio and audio stream
 - Create a custom voice while allow uploading multiple files at once
+
+## similar applications
+
+- Playht
+- Podcastle
--- app/page.js
+++ app/page.js
@@ -3,38 +3,75 @@ import styles from './page.module.css'
 const apiKey = process.env.XI_API_KEY
 
 export default function Home() {
+  const convertToPlain = (html) => {
+
+    // Create a new div element
+    const tempDivElement = document.createElement("div");
+
+    // Set the HTML content with the given value
+    tempDivElement.innerHTML = html;
+
+    // Retrieve the text property of the element
+    return tempDivElement.textContent || tempDivElement.innerText || "";
+  }
+
   const createAudio = (e) => {
-    e.preventDefault();
-    const ctx = new AudioContext();
-    let audio;
+    e.preventDefault()
+    const text = document.querySelector('#textInput').value
+    const audioElement = document.querySelector('#audio')
+    const audioLink = document.querySelector('#audio a')
+    const ctx = new AudioContext()
     console.log('Creating your audio..')
     // download an audio file
-
     fetch('https://api.elevenlabs.io/v1/text-to-speech/21m00Tcm4TlvDq8ikWAM', {
       method: 'POST',
       body: JSON.stringify({
-        'text': 'You got me waiting at the corner, just in the red light'
+        'text': text
       }),
       headers: {
         'accept': 'audio/mpeg',
         'xi-api-key': apiKey,
         'Content-Type': 'application/json'
       },
-    }).then(data => data.arrayBuffer())
-      .then(arrayBuffer => ctx.decodeAudioData(arrayBuffer))
-      .then(decodedAudio => {
-        audio = decodedAudio;
-        const playSound = ctx.createBufferSource();
-        playSound.buffer = audio;
-        playSound.connect(ctx.destination);
-        playSound.start(ctx.currentTime);
+    }).then(data => {
+      console.log(data)
+      return data.blob()
+    })
+      .then(blob => {
+        const audioURL = URL.createObjectURL(blob)
+        audioElement.src = audioURL
+        audioLink.href = audioURL
+        console.log(audioElement)
       })
       .catch((e) => console.log(e));
   }
+
+  const getText = (e) => {
+    e.preventDefault();
+    const inputUrl = document.querySelector('#url')
+    const url = new URL(inputUrl.value, 'https://dev.to/api/articles/')
+
+    fetch(url)
+      .then(res => res.json())
+      .then(json => {
+        const textarea = document.querySelector('#textInput')
+        textarea.value = convertToPlain(json.body_html)
+      })
+
+  }
+
   return (
     <main className={styles.main}>
+      <section>
+        <input size={100} type='text' id='url' defaultValue='luucamay/the-week-i-danced-with-martha-graham-and-unleashed-ai-magic-at-rc-2d1a' /><button onClick={getText}>Import from url</button>
+      </section>
+      <textarea id='textInput' rows={20} cols={40} defaultValue='It has been a while I am not who I was before.' />
       <button onClick={createAudio}>Create audio</button>
-
+      <div id='playerArea'>
+        <audio id='audio' controls src="none">
+          <a href="none"> Download audio </a>
+        </audio>
+      </div>
     </main>
   )
 }
